7d619dca3f023b6eb238bd30e74b4c4adb7cf5b9
Oliver Delaney <oliver.delaney@stu.barren.kyschools.us>
2023-09-20 09:41:03 -0500

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Oliver Delaney on 9/20/23.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
